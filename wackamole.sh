8230e766b650a55dfc3dffa137319a4340f19517
gingeropolous <gingeropolous@gmail.com>
2020-10-20 11:57:19 -0400

    increased wait time to 30 seconds. need to switch to array comparison next.

--- wackamole.sh
+++ wackamole.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+
+# Parameters to modify
 rpc_ip=127.0.0.1
 rpc_p=18081
 call_monerod=monerod
+p2pstate=~/.bitmonero/p2pstate.bin # This is the default, only change if you've changed yours
+
+
 cnct_monerod="$call_monerod --rpc-bind-ip $rpc_ip --rpc-bind-port $rpc_p"
-mon_service=MON_daemon
 work_dir=~/wacky_work
-p2pstate=~/.bitmonero/p2pstate.bin
 #Could get fancy with this and make it a systemd service as well
 start_fake="$call_monerod --data-dir $work_dir --no-sync --p2p-bind-port 18060 --rpc-bind-port 18061 --zmq-rpc-bind-port 18062 --out-peers 512"
 
@@ -39,27 +42,21 @@ echo "$cur_height"|od -xc
 
 cp $p2pstate $work_dir
 
-#sync_info
-#/monero-blockchain-import --pop-blocks 200
-
-# Need to call fake monerod in background
 echo "Starting fake"
 
 $start_fake --detach --pidfile $work_dir/save_pid.txt
-# echo $! > $work_dir/save_pid.txt
 
-sleep 5
+echo "Waiting...."
+sleep 30
 echo "Done waiting"
 # Fake directory is empty, fake is not syncing. Get output of sync_info and parse it for moles
 $start_fake sync_info > $work_dir/sync_info.txt
-#cat $work_dir/sync_info.txt
 # Delete the last line because there are brackets. 
 sed -n '5,$p' -i $work_dir/sync_info.txt
 sed -i '$d' $work_dir/sync_info.txt
 
-#cat $work_dir/sync_info.txt
-
 # This could be done by storing the sync_info into an array and then comparing cur_height to the values, but....
+# This will need to be changed to an array comparison if we need to wait 10 minutes to avoid false positives
 echo "grepping"
 grep -F "$cur_height" $work_dir/sync_info.txt | cut -f 1 -d ":" > $work_dir/new_moles.txt
 
@@ -74,7 +71,6 @@ sort $work_dir/old_moles.txt | uniq > $work_dir/new_moles.txt
 echo "Killing monerod"
 $start_fake exit
 #kill -15 `cat $work_dir/save_pid.txt`
-rm $work_dir/save_pid.txt
 sleep 3
 cat $work_dir/sync_info.txt
 echo "and the new_moles"
@@ -83,7 +79,7 @@ cat $work_dir/new_moles.txt
 echo "Adding new moles to monerod ban list"
 cat $work_dir/new_moles.txt | while read ip
 do
-$cnct_monerod ban $ip -1
+$cnct_monerod ban $ip
 done 
 
 echo "Starting over #####################"
